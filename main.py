7858ba8e52c7eee4a2d04836befff223d53c2ddb
Supportiii <89602269+Supportiii@users.noreply.github.com>
2021-08-31 21:07:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -189,14 +189,20 @@ async def callback_query(call: types.CallbackQuery):
             logger.info('#' + post_id + ': ' + get_formatted_username_or_id(target) + ' - access granted')
             await bot.answer_callback_query(call.id, body
                 .replace('{username}', get_formatted_username_or_id(target))
-                .replace('{name}', target.full_name)
+                .replace('{nutzername}', get_formatted_username_or_id(target))
+                .replace('{name}', target.full_name)                         
                 .replace('{uid}', 'id' + str(target.id))
+                .replace('{id}', 'id' + str(target.id))                            
                 .replace('{lang}', 'unknown' if target.language_code is None else target.language_code)
+                .replace('{sprache}', 'unknown' if target.language_code is None else target.language_code)
                 .replace('{pid}', '#' + post_id)
-                .replace('{ts}', str(creation_time))
+                .replace('{ts}', str(creation_time))                          
                 .replace('{now}', str(datetime.now()))
+                .replace('{jetzt}', str(datetime.now()))                           
                 .replace('{date}', datetime.now().strftime('%Y-%m-%d'))
-                .replace('{time}', datetime.now().strftime('%H:%M')),
+                .replace('{datum}', datetime.now().strftime('-%d.-%m.-%Y'))                           
+                .replace('{time}', datetime.now().strftime('%H:%M'))
+                .replace('{zeit}', datetime.now().strftime('%H:%M')),
                 True)
         else:
             logger.info('#' + post_id + ': ' + get_formatted_username_or_id(target) + ' - access denied')
