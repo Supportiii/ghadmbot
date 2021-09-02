ec6b8689a25334772c49aacab5f8e9ee494200a6
Supportiii <89602269+Supportiii@users.noreply.github.com>
2021-09-02 15:50:01 +0200

    Update resources.py

--- resources.py
+++ resources.py
@@ -54,8 +54,8 @@ class Keyboards:
                 types.InlineKeyboardButton('👥 Group',             url='https://t.me/iSupGr'),
                 types.InlineKeyboardButton('👷 Tests',             url='https://t.me/iTestGroups')],
                [types.InlineKeyboardButton('📝 HowTo',             url='https://telegra.ph/TGi-08-29')],
-               [types.InlineKeyboardButton('🌐 Official Bot Repo', url='https://github.com/undrcrxwn/hidethisbot')]])
-    
+               [types.InlineKeyboardButton('🌐 Official Bot Repo', url='https://github.com/undrcrxwn/hidethisbot')
+                types.InlineKeyboardButton('🌐 My Bot Repo',       url='https://github.com/Supportiii/hidethisbot')]])
     
 class Media:
     def group_greeting_sticker_id(self):
