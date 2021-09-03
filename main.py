8a6b9e08ff4168e8a8382be07ee13eb068449e1a
Supportiii <89602269+Supportiii@users.noreply.github.com>
2021-09-03 14:49:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,22 @@ from aiogram.utils.exceptions import BotBlocked, TelegramAPIError
 from os import getenv
 from sys import exit
 import logs
-from filters.py import IsGroupJoin
+
+from aiogram import types
+from aiogram.dispatcher.filters import BoundFilter
+
+
+class IsGroupJoin(BoundFilter):
+    key = "is_group_join"
+
+    def __init__(self, is_group_join: bool):
+        self.is_group_join = is_group_join
+
+    async def check(self, update: types.ChatMemberUpdated):
+        return update.old_chat_member.status in ("kicked", "left") and \
+               update.new_chat_member.status in ("member", "administrator") and \
+               update.chat.type in ("group", "supergroup")
+    
 
 logger.add(os.environ['LOG_PATH'], level = 'DEBUG')
 rsc = Resources(locales)
