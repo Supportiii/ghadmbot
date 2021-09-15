094d185ddbc58fbb99c227ce27de42a786cc84bb
Supporti <89602269+Supportiii@users.noreply.github.com>
2021-09-15 21:04:37 +0200

    Update locales.py

--- locales.py
+++ locales.py
@@ -99,17 +99,12 @@ locale_en.and_connector = 'and'
 locale_de.and_connector = '&'
 locale_it.and_connector = 'e'
 
-# ID_MESSAGE_GROUP
-locale_en.id_message_group = 'and'
-locale_de.id_message_group = '&'
-locale_it.id_message_group = 'e'
-
 # ID_MESSAGE
 locale_en.id_message = '👤 Your TG ID is {message.from_user.id}'
 locale_de.id_message = '👤 Deine Telegram ID ist {message.from_user.id}'
 locale_it.id_message = '👤 Your TG ID is {message.from_user.id}'
 
 # ID_MESSAGE_GROUP
-locale_en.id_message = '👥 This {message.chat.type} chat ID is {message.chat.id}.\n👤 Your ID is {message.from_user.id}.'
-locale_de.id_message = '👥 Die ID dieser {message.chat.type} ist {message.chat.id}.\n👤 Deine ID ist {message.from_user.id}.'
-locale_it.id_message = '👥 This {message.chat.type} chat ID is {message.chat.id}.\n👤 Your ID is {message.from_user.id}.'
+locale_en.id_message_group = '👥 This {message.chat.type} chat ID is {message.chat.id}.\n👤 Your ID is {message.from_user.id}.'
+locale_de.id_message_group = '👥 Die ID dieser {message.chat.type} ist {message.chat.id}.\n👤 Deine ID ist {message.from_user.id}.'
+locale_it.id_message_group = '👥 This {message.chat.type} chat ID is {message.chat.id}.\n👤 Your ID is {message.from_user.id}.'
