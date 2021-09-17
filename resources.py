d4ab284a2c278bbeb88f6d74042da7815279376c
emilianpvt <90937766+emilianpvt@users.noreply.github.com>
2021-09-18 01:06:34 +0200

    Update resources.py

--- resources.py
+++ resources.py
@@ -25,16 +25,6 @@ class QueryResults:
             description = body,
             thumb_url = 'https://i.imgur.com/hHIkDSu.png')
 
-    def mode_except(self, lang: str, post_id, body, scope_string):
-        keyboard = types.InlineKeyboardMarkup(inline_keyboard =
-            [[types.InlineKeyboardButton(self.locales[lang].view, callback_data = str(post_id) + ' except')]])
-        message_content = types.InputTextMessageContent(self.locales[lang].except_message % scope_string)
-        return types.InlineQueryResultArticle(
-            id = 'except', title = self.locales[lang].except_title % scope_string,
-            input_message_content = message_content,
-            reply_markup = keyboard,
-            description = body,
-            thumb_url = 'https://i.imgur.com/S6OZMHd.png')
 
     def spoiler(self, lang: str, post_id, body):
         keyboard = types.InlineKeyboardMarkup(inline_keyboard =
@@ -50,12 +40,7 @@ class QueryResults:
 class Keyboards:
     def info_keyboard(self):
         return types.InlineKeyboardMarkup(inline_keyboard=
-              [[types.InlineKeyboardButton('📝 HowTo',             url='https://telegra.ph/TGi-08-29')],
-               [types.InlineKeyboardButton('🌐 Official Bot Repo', url='https://github.com/undrcrxwn/hidethisbot'),
-                types.InlineKeyboardButton('🌐 My Bot Repo',       url='https://github.com/Supportiii/hidethisbot')],
-               [types.InlineKeyboardButton('📣 Channel',           url='https://t.me/iSupCh'),
-                types.InlineKeyboardButton('👥 Group',             url='https://t.me/iSupGr'),
-                types.InlineKeyboardButton('👷 TestGroups',        url='https://t.me/iTestGroups')]])
+              [types.InlineKeyboardButton('📝 HowTo',             url='https://telegra.ph/TGi-08-29')])
     
 class Media:
     def group_greeting_sticker_id(self):
