8cfa471abc275cd57ca3f0ec11a7fad6549ad184
Supporti <89602269+Supportiii@users.noreply.github.com>
2021-09-18 14:26:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ from resources import Resources
 
 import asyncio
 from asyncio import sleep
-    
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 logger.add(os.environ['LOG_PATH'], level = 'DEBUG')
 rsc = Resources(locales)
@@ -27,7 +29,84 @@ connection = psycopg2.connect(os.environ['DATABASE_URL'], sslmode = 'require')
 bot = Bot(token = os.environ['API_TOKEN'])
 dp = Dispatcher(bot)
 
-    
+#####################
+FayasNoushad = Client(
+    "Info-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+@FayasNoushad.on_message(filters.private & filters.command(["dc"]))
+async def start(bot, update):
+    text = START_TEXT.format(update.from_user.mention)
+    reply_markup = BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup,
+        quote=True
+    )
+
+@FayasNoushad.on_message((filters.private | filters.group) & filters.command(["info", "information"]))
+async def info(bot, update):
+    if (not update.reply_to_message) and ((not update.forward_from) or (not update.forward_from_chat)):
+        info = user_info(update.from_user)
+    elif update.reply_to_message and update.reply_to_message.forward_from:
+        info = user_info(update.reply_to_message.forward_from)
+    elif update.reply_to_message and update.reply_to_message.forward_from_chat:
+        info = chat_info(update.reply_to_message.forward_from_chat)
+    elif (update.reply_to_message and update.reply_to_message.from_user) and (not update.forward_from or not update.forward_from_chat):
+        info = user_info(update.reply_to_message.from_user)
+    else:
+        return
+    try:
+        await update.reply_text(
+            text=info,
+            reply_markup=BUTTONS,
+            disable_web_page_preview=True,
+            quote=True
+        )
+    except Exception as error:
+        await update.reply_text(error)
+
+def user_info(user):
+    text = "--**User Details:**--\n"
+    text += f"\n**First Name:** `{user.first_name}`"
+    text += f"\n**Last Name:** `{user.last_name},`" if user.last_name else ""
+    text += f"\n**User Id:** `{user.id}`"
+    text += f"\n**Username:** @{user.username}" if user.username else ""
+    text += f"\n**User Link:** {user.mention}" if user.username else ""
+    text += f"\n**DC ID:** `{user.dc_id}`" if user.dc_id else ""
+    text += f"\n**Is Deleted:** True" if user.is_deleted else ""
+    text += f"\n**Is Bot:** True" if user.is_bot else ""
+    text += f"\n**Is Verified:** True" if user.is_verified else ""
+    text += f"\n**Is Restricted:** True" if user.is_verified else ""
+    text += f"\n**Is Scam:** True" if user.is_scam else ""
+    text += f"\n**Is Fake:** True" if user.is_fake else ""
+    text += f"\n**Is Support:** True" if user.is_support else ""
+    text += f"\n**Language Code:** {user.language_code}" if user.language_code else ""
+    text += f"\n**Status:** {user.status}" if user.status else ""
+    text += f"\n\nMade by @SupVZ"
+    return text
+
+def chat_info(chat):
+    text = "--**Chat Details**--\n" 
+    text += f"\n**Title:** `{chat.title}`"
+    text += f"\n**Chat ID:** `{chat.id}`"
+    text += f"\n**Username:** @{chat.username}" if chat.username else ""
+    text += f"\n**Type:** `{chat.type}`"
+    text += f"\n**DC ID:** `{chat.dc_id}`"
+    text += f"\n**Is Verified:** True" if chat.is_verified else ""
+    text += f"\n**Is Restricted:** True" if chat.is_verified else ""
+    text += f"\n**Is Creator:** True" if chat.is_creator else ""
+    text += f"\n**Is Scam:** True" if chat.is_scam else ""
+    text += f"\n**Is Fake:** True" if chat.is_fake else ""
+    text += f"\n\nMade by @FayasNoushad"
+    return text
+
+FayasNoushad.run()
+ 
 def ignore(chat_id, timeout):
     ignored_chat_ids.add(chat_id)
     time.sleep(timeout)
