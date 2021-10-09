aacdb39383362fa4a3e95d63cb70bce4c48d3f92
Supporti <89602269+Supportiii@users.noreply.github.com>
2021-10-09 15:11:26 +0200

    Update resources.py

--- resources.py
+++ resources.py
@@ -1,6 +1,9 @@
 import random
+
 from aiogram import types
+
 from locales_dict import LocalesDict
+from models import PostMode
 
 class QueryResults:
     def __init__(self, locales: LocalesDict):
@@ -16,32 +19,35 @@ class QueryResults:
 
     def mode_for(self, lang: str, post_id, body, scope_string):
         keyboard = types.InlineKeyboardMarkup(inline_keyboard =
-            [[types.InlineKeyboardButton(self.locales[lang].view, callback_data = str(post_id) + ' for')]])
+            [[types.InlineKeyboardButton(self.locales[lang].view, callback_data = str(post_id) +
+            ' ' + PostMode.parse_key(PostMode.FOR))]])
         message_content = types.InputTextMessageContent(self.locales[lang].for_message % scope_string)
         return types.InlineQueryResultArticle(
-            id = 'for', title = self.locales[lang].for_title % scope_string,
+            id = str(PostMode.FOR), title = self.locales[lang].for_title % scope_string,
             input_message_content = message_content,
             reply_markup = keyboard,
             description = body,
-            thumb_url = 'https://tlgur.com/d/4AXbQl2g')
+            thumb_url = 'https://i.imgur.com/hHIkDSu.png')
 
     def mode_except(self, lang: str, post_id, body, scope_string):
         keyboard = types.InlineKeyboardMarkup(inline_keyboard =
-            [[types.InlineKeyboardButton(self.locales[lang].view, callback_data = str(post_id) + ' except')]])
+            [[types.InlineKeyboardButton(self.locales[lang].view, callback_data = str(post_id) +
+            ' ' + PostMode.parse_key(PostMode.EXCEPT))]])
         message_content = types.InputTextMessageContent(self.locales[lang].except_message % scope_string)
         return types.InlineQueryResultArticle(
-            id = 'except', title = self.locales[lang].except_title % scope_string,
+            id = str(PostMode.EXCEPT), title = self.locales[lang].except_title % scope_string,
             input_message_content = message_content,
             reply_markup = keyboard,
             description = body,
-            thumb_url = 'https://tlgur.com/d/gpYp6ZN4')
+            thumb_url = 'https://i.imgur.com/S6OZMHd.png')
 
-    def spoiler(self, lang: str, post_id, body):
+    def mode_spoiler(self, lang: str, post_id, body):
         keyboard = types.InlineKeyboardMarkup(inline_keyboard =
-            [[types.InlineKeyboardButton(self.locales[lang].view, callback_data = str(post_id) + ' spoiler')]])
+            [[types.InlineKeyboardButton(self.locales[lang].view, callback_data = str(post_id) +
+            ' ' + PostMode.parse_key(PostMode.SPOILER))]])
         message_content = types.InputTextMessageContent(self.locales[lang].spoiler_message)
         return types.InlineQueryResultArticle(
-            id = 'spoiler', title = self.locales[lang].spoiler_title,
+            id = str(PostMode.SPOILER), title = self.locales[lang].spoiler_title,
             input_message_content = message_content,
             reply_markup = keyboard,
             description = body,
@@ -50,18 +56,18 @@ class QueryResults:
 class Keyboards:
     def info_keyboard(self):
         return types.InlineKeyboardMarkup(inline_keyboard=
-              [[types.InlineKeyboardButton('📝 HowTo',             url='https://telegra.ph/TGi-08-29')],
-               [types.InlineKeyboardButton('🌐 Official Bot Repo', url='https://github.com/undrcrxwn/hidethisbot'),
-                types.InlineKeyboardButton('🌐 My Bot Repo',       url='https://github.com/Supportiii/hidethisbot')],
-               [types.InlineKeyboardButton('📣 Channel',           url='https://t.me/iSupCh'),
-                types.InlineKeyboardButton('👥 Group',             url='https://t.me/iSupGr'),
-                types.InlineKeyboardButton('👷 TestGroups',        url='https://t.me/iTestGroups')]])
-    
+             [[types.InlineKeyboardButton('🇺🇸 English',    url='https://teletype.in/@undrcrxwn/hidethisbot_en'),
+               types.InlineKeyboardButton('🇵🇱 Polski',     url='https://teletype.in/@undrcrxwn/hidethisbot_pl')],
+              [types.InlineKeyboardButton('🇷🇺 Русский',    url='https://teletype.in/@undrcrxwn/hidethisbot_ru'),
+               types.InlineKeyboardButton('🇺🇦 Українська', url='https://teletype.in/@undrcrxwn/hidethisbot_ua')],
+              [types.InlineKeyboardButton('🇮🇹 Italiano',   url='https://teletype.in/@undrcrxwn/hidethisbot_it'),
+               types.InlineKeyboardButton('🇨🇿 Čeština',    url='https://teletype.in/@undrcrxwn/hidethisbot_cz')]])
+
 class Media:
     def group_greeting_sticker_id(self):
-        return random.choice(('CAACAgIAAxkBAAEpCbphK_C_---mCsuQDAkH1L7Efjx6HAACwQ0AAv86UUvmahUYxf0RgiAE',
-                              'CAACAgIAAxkBAAEpCb9hK_DbX8gXxdXD-w_ZOj0GM2Et_wACRRIAAu8nWEt3iTB_7h6DdCAE',
-                              'CAACAgIAAxkBAAEpCcFhK_DvPmbR3gi-fp4oKe7OXgsBTwACZQ4AAuB3WEvVwmBch1xaqSAE'))
+        return random.choice(('CAACAgIAAxkBAAECkihg7Y5tYnlKz9jRe6QCNOyvEZri2wACSQ4AAliyaUuDPYCgY_2GXiAE',
+                              'CAACAgIAAxkBAAECkilg7Y5tzJPtIX4UMDgYaoxD6zcrogAC8Q0AAvMraEvkpXQDG5qEbyAE',
+                              'CAACAgIAAxkBAAECkipg7Y5tQk6MZlccqoudX9PEnxPbUwACfBAAAhJpcEuU9SdfdRAPdiAE'))
 
 class Resources:
     def __init__(self, locales: LocalesDict):
